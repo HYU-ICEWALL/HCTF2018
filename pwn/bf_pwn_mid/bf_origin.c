89761a7ced9568f3ef4769dea05dcb7381befc00
2017036344 <delsponn@gmail.com>
2019-11-07 21:33:50 +0900

    [Added] Mid Pwnable

---
+++ pwn/bf_pwn_mid/bf_origin.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+static char origin[0x20] = "HCTF{B0RING_T1M1NG_1SSU3}";
+
+
+void init()
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+}
+
+int compare(char *pwd)
+{
+	for(int i = 0; strlen(origin) ; i ++)
+	{
+		sleep(1);
+		if(pwd[i] == origin[i])
+		{
+			continue;
+		}
+		else
+		{
+			printf("Wrong Password!\n");
+			return 0;
+		}
+	}
+	printf("Correct Password!\n");
+}
+
+
+int main()
+{
+	char password[0x20];
+	init();
+	printf("Input Password [A-Z0-9_{}]\n");
+
+	read(0, password, sizeof(password));
+
+	compare(password);
+}
---
+++ pwn/bf_pwn_mid/dockerrun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run --name bf -dt -p 1002:1002 bf
